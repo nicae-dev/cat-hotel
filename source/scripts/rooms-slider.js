7075fc109b115d6b702e30c2ad6d045fa8c476d9
Veronica Efimova <nica.e@yandex.ru>
2025-08-12 15:24:16 +0300

    Свертины и стилизованы все карточки комнат-номеров, сделан слайдер картинок в карточке (swiper в vendor). Изменены размеры картинок svg

---
+++ source/scripts/rooms-slider.js
@@ -0,0 +1,19 @@
+
+// import Swiper bundle with all modules installed
+// import Swiper from 'swiper/bundle';
+
+// import styles bundle
+// import 'swiper/swiper-bundle.min.css';
+
+// init Swiper:
+const swiperRooms = new Swiper('.room__slider', {
+  direction: 'horizontal',
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+// console.log('rooms-slider.js');
+// console.log(swiperRooms);
