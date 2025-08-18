1ca1951d18fea79dd01f9579b9a4db34266d5c9d
Veronica Efimova <nica.e@yandex.ru>
2025-08-18 17:02:44 +0300

    доделывает оставшийся макет, подгоняет под pixel perfect, приводит к единообразию имена фото

--- source/scripts/fears-list.js
+++ source/scripts/fears-list.js
@@ -1,9 +1,8 @@
 const buttons = document.querySelectorAll('.fears__question');
 
-buttons.forEach((button)=>button.addEventListener('click', (event) =>{
+buttons.forEach((button)=>button.addEventListener('click', () =>{
   const parent = button.closest('.fears__wrap');
   const answer = parent.querySelector('.fears__answer');
-  console.log(answer);
   answer.classList.toggle('fears__answer--opened');
   button.classList.toggle('fears__question--close');
 }));
